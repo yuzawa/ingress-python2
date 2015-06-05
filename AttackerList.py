8fc1cdb5d174cd772252c91264f1a98e30526c4a
Manabu Yuzawa <yuzawa@waltz.plala.or.jp>
2015-06-05 20:16:01 +0900

    Finished second version

--- AttackerList.py
+++ AttackerList.py
@@ -11,17 +11,16 @@ def main():
     api = GmailApi()
 
     user = "me"
-#    qstring = "subject:Ingress Damage Report: Entities attacked by"
-    qstring = "from:moriy-tky@umin.ac.jp"
+    qstring = "subject:Ingress Damage Report: Entities attacked by"
     number = "10"
     token = ""
 
     while 1:
 #    for i in range(0,1):
 
-        print ""
-        print 'token: ',
-        print token
+#        print ""
+#        print 'token: ',
+#        print token
 
         list = api.getMailList(user, qstring, number, token)
 
@@ -30,24 +29,32 @@ def main():
         if not messages:
             print 'No Messages found.'
         else:
-            print 'Messages exists'
+#            print 'Messages exists'
             for message in messages:
                 message = api.getMailBody(user, message['id'])
-#                print(message["snippet"].split(":")[3].split(" ")[3]),
-#                print(','),
+                print(message["snippet"].split(":")[3].split(" ")[3]),
+#                print(message["snippet"]),
+                print(','),
+
 #                print(message["payload"]["headers"][16]["value"].split(" ")[6]),
 #                print(','),
 #                print(message["payload"]["headers"][15]["value"])
 #                print(message["snippet"])
                 for header in message["payload"]["headers"]:
                     if header["name"] == "Subject":
-                        print header["value"]
-                    if header["name"] == "Date":
-                        print header["value"]
+                        print(header["value"].split(" ")[6]),
+#                    if header["name"] == "Date":
+#                        print header["value"],
+                print ""
 
         token = list.get('nextPageToken')
-        print ""
-        if token == "":
+#        print ""
+
+#        print ""
+#        print "token at end: ",
+#        print token
+        if not token:
+#            print "match!"
             break
 
 if __name__ == '__main__':
