2ece61f156580b2ecf9751c17d3a8f3cb41bcde4
Manabu Yuzawa <yuzawa@waltz.plala.or.jp>
2015-06-05 11:53:44 +0900

    First Commit
    Minimum Python

---
+++ AttackerList.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.insert(1, '/Library/Python/2.7/site-packages')
+
+def main():
+    print 'Hello World!'
+
+if __name__ == '__main__':
+    main()
