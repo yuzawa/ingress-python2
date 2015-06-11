9c5ac9e299ce08904933329bab59445d45233f15
Manabu Yuzawa <yuzawa@waltz.plala.or.jp>
2015-06-11 18:33:35 +0900

    Add Analyze 3
    analyze_html finished

--- AttackerList.py
+++ AttackerList.py
@@ -15,6 +15,67 @@ from attackreportparser import AttackReportParser
 def analyze_html(reportHtml):
     print "Inside analyze_html"
 
+    parser = AttackReportParser()
+    parser.feed(reportHtml.decode('utf-8'))
+    parser.close()
+
+    print parser.strings
+
+    myName = parser.strings[1]
+    myFaction = parser.strings[3]
+    myLevel = parser.strings[5]
+    attackedPortal = parser.strings[7]
+#    attackedTime = onechord.strftime("%Y/%m/%d %H:%M:%S")
+
+    parsedLength = len(parser.strings)
+    print parsedLength
+
+    if "LINK" in parser.strings[9]:
+        attackedType = "Link"
+        linkedPortal = parser.strings[12]
+        address = parser.strings[13]
+        attacker = parser.strings[14]
+        if "uncaptured" in parser.strings[parsedLength-1]:
+            owner = ""
+        else:
+            owner = parser.strings[parsedLength-1]
+
+    elif "Resonator" in parser.strings[15]:
+        attackedType = "Resonator and Mod"
+        address = parser.strings[10]
+        attacker = parser.strings[16]
+        if "uncaptured" in parser.strings[20]:
+            owner = ""
+        else:
+            owner = parser.strings[21]
+
+    elif "Resonator" in parser.strings[10]:
+        attackedType = "Resonator"
+        address = parser.strings[8]
+        attacker = parser.strings[11]
+        if "uncaptured" in parser.strings[17]:
+            owner = ""
+        else:
+            owner = parser.strings[18]
+
+    elif "Mod" in parser.strings[10]:
+        attackedType = "Mod"
+        address = parser.strings[8]
+        attacker = parser.strings[11]
+        if "uncaptured" in parser.strings[17]:
+            owner = ""
+        else:
+            owner = parser.strings[18]
+
+    print attackedType
+    print myName
+    print attacker
+    print myLevel
+    print attackedPortal
+    print owner
+
+
+
 
 def main():
 
@@ -39,74 +100,32 @@ def main():
         if not messages:
             print 'No Messages found.'
         else:
-#            print 'Messages exists'
             for message in messages:
                 print ""
                 print message['id']
                 message = api.getMailBody(user, message['id'])
                 for header in message["payload"]["headers"]:
                     if header["name"] == "Subject":
-#                        print(header["value"].split(" ")[6]),
                         attackAgent = header["value"].split(" ")[6]
                     if header["name"] == "Date":
-#                        Tue, 09 Jun 2015 04:00:19
                         mailTime = datetime.datetime.strptime(header["value"], "%a, %d %b %Y %H:%M:%S +0000")
-#                        print header["value"],
                 print ""
                 for part in message["payload"]["parts"]:
 
-#                    print message
                     for bodyHeader in part["headers"]:
-#                        if bodyHeader["name"] == "Content-Transfer-Encoding":
-#                            print "body header: ",
-#                            print bodyHeader["value"]
 #                    if bodyHeader["value"] == "base64":
 #                        print "Orignal base64"
 #                        print part["body"]["data"]
 #                        print "now base64 decoding"
 #                        print base64.urlsafe_b64decode(part["body"]["data"].encode("utf-8"))
                         if bodyHeader["value"] == "quoted-printable":
-#                        print "Orignal quoted-printable"
-#                            print part["body"]["data"]
-#                        print "now qulted-printable decoding"
                             reportHtml = base64.urlsafe_b64decode(part["body"]["data"].encode("utf-8"))
 #                            print reportHtml
 
                             analyze_html(reportHtml)
 
-                            parser = AttackReportParser()
-                            parser.feed(reportHtml.decode('utf-8'))
-                            parser.close()
-
-                            print parser.strings
-
-                            if "DAMAGE:" in parser.strings[9]:
-                                myName = parser.strings[1]
-                                myFaction = parser.strings[3]
-                                myLevel = parser.strings[5]
-                                attackedPortal = parser.strings[7]
-                                address = parser.strings[8]
-                                attacker = parser.strings[11]
-                                if "uncaptured" in parser.strings[17]:
-                                    owner = ""
-                                else:
-                                    owner = parser.strings[18]
-
-#                            print reportHtml
-
-                damageReport = True
-
-                if damageReport:
-                    print mailTime.strftime("%Y/%m/%d %H:%M:%S")
-                    print attacker
-                    print attackedPortal
-                    print address
-#                    print owner.replace("Owner: ", "")
-                    print owner
-
         token = list.get('nextPageToken')
         if not token:
-#            print "match!"
             break
 
 if __name__ == '__main__':
